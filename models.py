c2b80324b3718f5e9471d34c318a5893128d3872
shreeshyamevents <shreeshyameventsharda@gmail.com>
2026-09-09 18:45:29 +0530

    Add Flask-Login properties to User model

--- models.py
+++ models.py
@@ -27,6 +27,22 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
+    # Flask-Login expects these properties
+    @property
+    def is_authenticated(self):
+        return True
+    
+    @property
+    def is_active(self):
+        return True
+    
+    @property
+    def is_anonymous(self):
+        return False
+    
+    def get_id(self):
+        return str(self.id)
+    
     def __repr__(self):
         return f'<User {self.mobile}>'
 
